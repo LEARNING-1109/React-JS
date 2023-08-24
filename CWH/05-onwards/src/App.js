fb8a6c8f7a9d2504612de145a0f0ebcea8ed735b
ayush_kr <hellotoayush@gmail.com>
2023-08-24 12:20:27 +0530

    Feature added to Text Utils : Dark Mode

--- CWH/05-onwards/src/App.js
+++ CWH/05-onwards/src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+// abb: imrs
+import React, { useState } from 'react';
+
 function App() {
+  const [darkMode, setDarkMode] = useState("light");
+
+  const toggleMode = () => {
+    console.log('Toggle is clicked');
+    if (darkMode === "light") {
+      setDarkMode('dark');
+    }
+    else {
+      setDarkMode('light');
+    }
+  }
+
   return (
     <>
       {/* <Navbar title="iTextUtils" aboutText="iAboutUs" /> */}
       {/* <Navbar title={45} aboutText="iAboutUs" /> */}
-      <Navbar />
-      <TextForm heading='Enter the Text' />
+      <Navbar drk_mode={darkMode} toggleMode={toggleMode} />
+      <TextForm heading='Enter the Text' drk_mode={darkMode} />
     </>
     
   );
--- CWH/05-onwards/src/components/TextForm.js
+++ CWH/05-onwards/src/components/TextForm.js
@@ -1,16 +1,15 @@
-// Hare krishna
 import React from 'react';
 import { useState } from 'react';
 
-export default function TextForm(props) {    
+export default function TextForm(props) {
     // const [text, setText] = useState("");    
-    const [text, setText] = useState(localStorage.getItem('saveTxt'));    
+    const [text, setText] = useState(localStorage.getItem('saveTxt'));
 
     // 🗃️saving to local storage -------------------
     const saveToLocalStorage = (value) => {
         localStorage.setItem('saveTxt', value);
     }
-    
+
     const handleClearClick = () => {
         let newTxt = '';
         setText(newTxt);
@@ -78,18 +77,18 @@ export default function TextForm(props) {
             }
         }
     }
-    const handleAlternatingClick = () => {        
-        let alterTxt = "";        
-        
-        for (let i = 0; i < text.length; i++){
+    const handleAlternatingClick = () => {
+        let alterTxt = "";
+
+        for (let i = 0; i < text.length; i++) {
             if (i % 2 === 0) {
-                alterTxt += text.charAt(i).toLowerCase();                
+                alterTxt += text.charAt(i).toLowerCase();
             }
             else {
                 alterTxt += text.charAt(i).toUpperCase();
             }
         }
-        console.log(text);        
+        console.log(text);
         setText(alterTxt);
         console.log(text);  // ❓ why the value of text is not updating instantly 
         saveToLocalStorage(alterTxt);
@@ -102,37 +101,41 @@ export default function TextForm(props) {
 
     // 🔄️ word count and character count --------------------------------
     let wordCount;
-    wordCount = text.split(' ').length;    
+    wordCount = text.split(' ').length;
     // console.log(text.split(' '));
     // console.log(wordCount);
 
     let characterCount = text.length;
     return (
         <>
-            <div className='container my-1 '>
-                <div className='mb-2'>
-                    <label htmlFor="exampleFormControlTextarea1"><h3>{props.heading}</h3></label>
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="5" placeholder='Enter your text ...' ></textarea>
-                </div>
+            <div className={`main-container p-4 text-${props.drk_mode === 'light' ? 'dark' : 'light'} bg-${props.drk_mode}`} style={{
+                height: "90vh"
+            }}>
+                <div className='container my-1'>
+                    <div className='mb-2'>
+                        <label htmlFor="exampleFormControlTextarea1"><h3>{props.heading}</h3></label>
+                        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="5" placeholder='Enter your text ...' ></textarea>
+                    </div>
 
-                {/* 🆗 Buttons -------------------- */}
-                <div className='d-flex flex-wrap justify-content-between align-content-around' >
-                    <button className="btn btn-danger" onClick={handleClearClick} data-toggle="tooltip" title="Clear Input Field" >Clear All</button>
-                    <button className="btn btn-primary" onClick={handleUpClick}>CONVERT TO UPPERCASE</button>
-                    <button className="btn btn-success" onClick={handleLoClick}>convert to lowercase</button>
+                    {/* 🆗 Buttons -------------------- */}
+                    <div className='d-flex flex-wrap justify-content-between align-content-around'  >
+                        <button className="btn btn-danger" onClick={handleClearClick} data-toggle="tooltip" title="Clear Input Field" >Clear All</button>
+                        <button className="btn btn-primary" onClick={handleUpClick}>CONVERT TO UPPERCASE</button>
+                        <button className="btn btn-success" onClick={handleLoClick}>convert to lowercase</button>
 
-                    <button className="btn btn-success" onClick={handleCapitalizedClick}>Capitalized Case</button>
-                    <button className="btn btn-info" onClick={handleInverseClick}>iNVERSE cASE</button>
-                    <button className="btn btn-dark" onClick={handleAlternatingClick} >aLtErNaTiNg cAsE</button>
-                    <button className="btn btn-primary" disabled onClick={handleTitleClick} data-toggle="tooltip" title="Capitalization of the first word, and all other words, except for articles, prepositions, and conjunctions.This is an Example of Title Case in Use." >Title Case</button>
+                        <button className="btn btn-success" onClick={handleCapitalizedClick}>Capitalized Case</button>
+                        <button className="btn btn-info" onClick={handleInverseClick}>iNVERSE cASE</button>
+                        <button className="btn btn-primary" onClick={handleAlternatingClick} >aLtErNaTiNg cAsE</button>
+                        <button className="btn btn-primary" disabled onClick={handleTitleClick} data-toggle="tooltip" title="Capitalization of the first word, and all other words, except for articles, prepositions, and conjunctions.This is an Example of Title Case in Use." >Title Case</button>
+                    </div>
                 </div>
-            </div>
-            <div className="container my-2">
-                <h3>Text Summay</h3>
-                <p>{wordCount - 1} words & {characterCount} characters</p>
-                <p>Time to read the Whole Content : {0.2 * wordCount} seconds</p>
-                <h3>Preview</h3>
-                {text}
+                <div className="container my-2">
+                    <h3>Text Summay</h3>
+                    <p>{wordCount - 1} words & {characterCount} characters</p>
+                    <p>Time to read the Whole Content : {0.2 * wordCount} seconds</p>
+                    <h3>Preview</h3>
+                    {text}
+                </div>
             </div>
         </>
     )
